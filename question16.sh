8432c8890d1bae12c045d32c264cd72056516f95
bbootwala <bbootwala@grepruby.io>
2023-06-01 12:27:33 +0930

    final

--- question16.sh
+++ question16.sh
@@ -1,6 +1,6 @@
 git fetch origin 
+git checkout branch3
 git checkout branch2
 git merge branch3 --no-edit
-git checkout branch3
 git checkout main
 git branch -D branch3
--- question17.sh
+++ question17.sh
@@ -3,8 +3,8 @@ git checkout -b "branch1"
 touch ./newfile1
 mv dir1/dir2/foo dir1/foo
 rm -rf dir1/dir2
-git add newfile1 dir1/
-git commit -b "branch1"
+git add .
+git commit -m "branch1"
 git stash
 git checkout main
 git checkout -b "branch2"
@@ -12,7 +12,8 @@ cp dir1/dir2/foo dir1/dir2/foo_modified
 rm dir1/dir2/foo
 mkdir dir1/dir3
 touch dir1/dir3/newfile2
-git add dir1/
+rm -rf dir3/
+git add .
 git commit -m "branch2"
 git checkout main
 cp dir3/bar dir3/bar_copy
